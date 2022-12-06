fc1edf777ba3483c2fe51adfd0a55231c06359d3
DarKrone <Brener-2000@mail.ru>
2022-12-06 18:42:24 +0400

    каталог чуть более проработан

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 const filterBox = document.querySelectorAll('.box');
 document.querySelector('.filters').addEventListener('click', event => {
-    if (event.target.tagName !== 'LI') return false;
+    if (event.target.tagName != 'BUTTON') return false;
 
-    let filterClass = event.target.dataset['f'];
+    let filterClass = getValue();
     filterBox.forEach(elem => {
         elem.classList.remove('hide');
         if (!elem.classList.contains(filterClass) && filterClass != 'all'){
@@ -10,3 +10,9 @@ document.querySelector('.filters').addEventListener('click', event => {
         }
     });
 });
+
+function getValue() {
+    var select = document.getElementById("select_1");
+    var value = select.value;
+	return value;
+}
